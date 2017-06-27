4b62ab0529c7a14406cf3cb93b9a22806d8d372b
Raymond Boswel <raymondboswel@gmail.com>
2017-06-27 09:05:28 +0200

    Updated return value from method

--- lib/extwitter/oauth.ex
+++ lib/extwitter/oauth.ex
@@ -63,7 +63,7 @@ defmodule ExTwitter.OAuth do
     # send_httpc_request(:post, request, options)
     {status, response} = HTTPoison.post(url, body, [header], [connect_timeout: 50000, recv_timeout: 50000, timeout: 50000])
     Logger.warn "Post result: #{inspect response}"
-    response.body
+    {:ok, {response, response.headers, response.body}}
   end
 
   def send_httpc_request(method, request, options) do
