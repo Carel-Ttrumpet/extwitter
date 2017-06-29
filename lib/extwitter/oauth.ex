f0c1b23da312db808c9fdd27b570c7d7f42bbfab
Raymond Boswel <raymondboswel@gmail.com>
2017-06-29 14:45:28 +0200

    Added log

--- lib/extwitter/oauth.ex
+++ lib/extwitter/oauth.ex
@@ -76,6 +76,7 @@ defmodule ExTwitter.OAuth do
     signed_params = get_signed_params("post", media_url, [], consumer_key, consumer_secret, access_token, access_token_secret)
     {header, req_params} = OAuther.header(signed_params)
     %{size: size} = File.stat! path
+    Logger.warn "Size: #{inspect size}"
     result = HTTPoison.post!("https://upload.twitter.com/1.1/media/upload.json",
                              {:multipart,
                               [ {:file, path},
