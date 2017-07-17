334ceeef8101572d046292957cfe8a61a9c0489b
Raymond Boswel <raymondboswel@gmail.com>
2017-07-17 15:58:20 +0200

    Fixed map reference

--- lib/extwitter/api/base.ex
+++ lib/extwitter/api/base.ex
@@ -34,7 +34,7 @@ defmodule ExTwitter.API.Base do
 
     response = do_request(:post, "https://upload.twitter.com/1.1/media/upload.json", [command: "INIT", total_bytes: size, media_type: content_type])
     Logger.warn "Media INIT response: #{inspect response}"
-    media_id = response |> Poison.decode! |> Map.fetch!("media_id")
+    media_id = response[:media_id]
     stream = File.stream!(path, [], 2048)
     Enum.reduce(stream, 0, fn(chunk, acc) ->
       res = do_request(:post, "https://upload.twitter.com/1.1/media/upload.json", [command: "APPEND", media_id: media_id, media: chunk])
